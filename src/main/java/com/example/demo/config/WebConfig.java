f536f75189891d9141a6f5bf689e2573d0a9ee95
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-09 21:59:34 +0100

    update WebConfig class

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	
@@ -11,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry corsRegistry) {
 		
 		corsRegistry.addMapping("/**")
-					.allowedOrigins("*")
+					.allowedOriginPatterns("*")
 					.allowedMethods("*")
 					.maxAge(3600L)
 					.allowedHeaders("*")
